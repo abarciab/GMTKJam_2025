4f3ebc5d291a6433f2fc7c6b3a3f8040f5245a30
Voads <blue.j.row@gmail.com>
2025-08-01 20:24:09 -0700

    area1 prefab update, corrupted trees prefabs (just the models in the prefabs with the scriptable spawner object)

--- Assets/Scripts/ScatterObjects.cs
+++ Assets/Scripts/ScatterObjects.cs
@@ -13,7 +13,51 @@ public class ScatterObjects : MonoBehaviour
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private Transform _permantentParent;
 
+    [SerializeField] private List<float> _prefabProbabilities = new List<float>();
+
     private List<GameObject> _spawnedObjects = new List<GameObject>();
+
+    private void OnValidate()
+    {
+        // Ensure the probabilities list matches the number of prefabs
+        if (_prefabProbabilities.Count != _prefabs.Count)
+        {
+            while (_prefabProbabilities.Count < _prefabs.Count)
+            {
+                _prefabProbabilities.Add(1f); // Default probability
+            }
+            while (_prefabProbabilities.Count > _prefabs.Count)
+            {
+                _prefabProbabilities.RemoveAt(_prefabProbabilities.Count - 1);
+            }
+        }
+    }
+
+    private GameObject GetRandomPrefab()
+    {
+        if (_prefabs.Count == 0 || _prefabProbabilities.Count == 0) return null;
+
+        // Normalize probabilities
+        float totalProbability = 0f;
+        foreach (var probability in _prefabProbabilities)
+        {
+            totalProbability += probability;
+        }
+
+        float randomValue = Random.Range(0f, totalProbability);
+        float cumulativeProbability = 0f;
+
+        for (int i = 0; i < _prefabs.Count; i++)
+        {
+            cumulativeProbability += _prefabProbabilities[i];
+            if (randomValue <= cumulativeProbability)
+            {
+                return _prefabs[i];
+            }
+        }
+
+        return null; // Fallback (shouldn't happen)
+    }
 
     [ButtonMethod]
     public void GenerateObjects()
@@ -68,12 +112,16 @@ public class ScatterObjects : MonoBehaviour
         var pos = new Vector3(circlePos.x + transform.position.x, transform.position.y, circlePos.y + transform.position.z);
 
         var didHit = Physics.Raycast(pos, Vector3.down, out var hitInfo, 10000, _groundLayer);
-        if (didHit) {
-            var selectedObject = _prefabs[Random.Range(0, _prefabs.Count)];
-            var newObject = Instantiate(selectedObject, hitInfo.point,Quaternion.Euler(0, Random.Range(0, 360), 0), transform);
-            newObject.transform.localScale = Vector3.one * Random.Range(_scaleLimits.x, _scaleLimits.y);
-            _spawnedObjects.Add(newObject);
-            Utils.SetDirty(newObject);
+        if (didHit)
+        {
+            var selectedObject = GetRandomPrefab();
+            if (selectedObject != null)
+            {
+                var newObject = Instantiate(selectedObject, hitInfo.point, Quaternion.Euler(0, Random.Range(0, 360), 0), transform);
+                newObject.transform.localScale = Vector3.one * Random.Range(_scaleLimits.x, _scaleLimits.y);
+                _spawnedObjects.Add(newObject);
+                Utils.SetDirty(newObject);
+            }
         }
         else {
             //var selectedObject = _prefabs[Random.Range(0, _prefabs.Count)];
